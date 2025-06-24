7aa0d70a2e0f124eb2093dd4cb27736b6a3b5eba
AndreaCatapano <a.catapano96@gmail.com>
2025-06-24 18:02:29 +0200

    first commit

---
+++ src/com/ferragnez/party/CheckGuest.java
@@ -0,0 +1,18 @@
+package com.ferragnez.party;
+
+import java.util.Scanner;
+
+public class CheckGuest {
+
+    public static void main(String[] args) {
+        String[] guestList = { "dua-lipa", "paris-hilton", "manuel-agnelli", "j-ax", "francesco-totti", "ilary-blasi",
+                "bebe-vio", "luis", "parids-zarei", "martina-maccherone", "rachel-zeilic" };
+        Scanner input = new Scanner(System.in);
+
+        String userAttempt;
+
+        System.out.print("Inserisci il tuo nome e cognome: ");
+        userAttempt = input.nextLine();
+    }
+
+}
